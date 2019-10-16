7f8c016c52c7a309f617445016eeebe5cc8c3eda
Rahul Kumar Singh <singhrahulkr1002@gmail.com>
2019-10-16 06:52:37 +0530

    added user servise

---
+++ src/main/java/com/demo/swagger/users/Interf.java
@@ -0,0 +1,6 @@
+package com.demo.swagger.users;
+@FunctionalInterface
+public interface Interf {
+    public void m();
+
+}
---
+++ src/main/java/com/demo/swagger/users/UserController.java
@@ -0,0 +1,45 @@
+package com.demo.swagger.users;
+
+
+import java.net.URI;
+import java.util.List;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserDaoService service;
+    //GET /users
+    // retrieveAllUsers
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers()
+    {
+        return service.findALL();
+    }
+
+    //GET /user/{id}
+    //retrieveUser
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        User user =service.findOne(id);
+        return user;
+    }
+    
+    @PostMapping("/users")
+    public ResponseEntity<String> createUser(@RequestBody User user){
+        User savedUser=service.save(user);
+        URI location=ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+        return ResponseEntity.created(location).build();
+    }
+}
